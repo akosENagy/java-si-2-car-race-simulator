27b8bd271c99d5ed758e0051cb63470fd2443570
akosENagy <akos.e.nagy@gmail.com>
2017-08-23 14:20:39 +0200

    Created skeletons with constructors. Needs methods and main stuff.

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@ public class Main {
     static boolean isRaining;
 
     public static void main(String[] args) {
-        Car c1 = new Car();
-        Car c2 = new Car();
 
-        System.out.println(c1.name + " " + c1.normalSpeed);
-        System.out.println(c2.name + " " + c2.normalSpeed);
+        Vehicle[] vehicles = new Vehicle[5];
+        vehicles[0] = new Car();
+        vehicles[1] = new Motorcycle();
+        vehicles[2] = new Car();
+        vehicles[3] = new Motorcycle();
+        vehicles[4] = new Truck();
+
+
+        for (Vehicle v : vehicles) {
+            System.out.println(v.name + " " + v.distanceTravelled + " ");
+        }
     }
-
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,5 @@
+abstract class Vehicle {
+    String name;
+    int distanceTravelled = 0;
+    abstract void moveForAnHour();
+}
