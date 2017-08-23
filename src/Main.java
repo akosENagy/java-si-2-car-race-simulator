498f7ceeda566da2342da324436da12c6739d10b
akosENagy <akos.e.nagy@gmail.com>
2017-08-23 14:00:57 +0200

    Added Car without methods and empty class files for the others

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
+    static boolean isRaining;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Car c1 = new Car();
+        Car c2 = new Car();
+
+        System.out.println(c1.name + " " + c1.normalSpeed);
+        System.out.println(c2.name + " " + c2.normalSpeed);
     }
+
 }
